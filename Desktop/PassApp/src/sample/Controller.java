7096e7ce9eb93e6441469709d329cd66369d811e
Brad <bradstell87@gmail.com>
2016-02-05 18:39:37 -0500

    Added source add button

--- Desktop/PassApp/src/sample/Controller.java
+++ Desktop/PassApp/src/sample/Controller.java
@@ -2,45 +2,141 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    Pane mainPane;
-
-    @FXML
-    AnchorPane mainAnchorPane;
+public class Controller implements Initializable {
 
     @FXML
-    ScrollPane mainScrollPane;
+    ScrollPane sourceScrollPane;
 
-    public void initialize() throws Exception {
+    @FXML
+    VBox sourceScrollBox;
 
-        addTile();
+    @FXML
+    ScrollPane accountScrollPane;
+
+    @FXML
+    StackPane accountStackPane;
+
+    @FXML
+    VBox accountScrollBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        bindComponents();
+        addAccountAddButton();
+        fillInTestTiles();
     }
 
-    private void addTile() throws Exception {
+    @FXML
+    public void sourceAddMouseClicked() {
+
+    }
+
+    @FXML
+    public void sourceRemoveClicked() {
+
+    }
+
+    @FXML
+    public void sourceEditClicked() {
+
+    }
+
+    private void addAccountAddButton() {
+
+        StackPane sp = new StackPane();
+        sp.setMaxWidth(40.0f);
+        sp.setMaxHeight(40.0f);
+        sp.setPadding(new Insets(0.0f, 10.0f, 20.0f, 0.0f));
+
+        Circle circle = new Circle();
+        circle.setRadius(30.0f);
+        circle.setFill(Color.CYAN);
+        circle.setEffect(new DropShadow(40.0f, 10.0f, 10.0f, Color.BLACK));
+
+        Line vert = new Line();
+        vert.setStartX(17.0f);
+        vert.setStartY(3.0f);
+        vert.setEndX(17.0f);
+        vert.setEndY(37.0f);
+        vert.setStroke(Color.WHITE);
+        vert.setStrokeWidth(10.0f);
+
+        Line horz = new Line();
+        horz.setStartX(3.0f);
+        horz.setStartY(17.0f);
+        horz.setEndX(37.0f);
+        horz.setEndY(17.0f);
+        horz.setStroke(Color.WHITE);
+        horz.setStrokeWidth(10.0f);
+
+        sp.getChildren().add(circle);
+        sp.getChildren().add(vert);
+        sp.getChildren().add(horz);
+
+
+        accountStackPane.getChildren().add(sp);
+        StackPane.setAlignment(sp, Pos.BOTTOM_RIGHT);
+    }
+
+    public void bindComponents() {
+        sourceScrollBox.prefWidthProperty().bind(sourceScrollPane.widthProperty());
+        sourceScrollBox.prefHeightProperty().bind(sourceScrollPane.heightProperty());
+
+        accountStackPane.prefWidthProperty().bind(accountScrollPane.widthProperty());
+        accountStackPane.prefHeightProperty().bind(accountScrollPane.heightProperty());
+    }
+
+    public void fillInTestTiles() {
+
+        loadSourceData();
+        loadAccountData();
+    }
+
+    public void loadSourceData() {
 
         for (int i = 0; i < 20; i++) {
-            StackPane tile = FXMLLoader.load(getClass().getResource("tile.fxml"));
+            HBox tile;
 
-            Rectangle rect = new Rectangle(tile.getWidth(), tile.getHeight());
-            rect.setFill(null);
-            rect.setStroke(Color.BLACK);
-            rect.widthProperty().bind(tile.widthProperty());
-            rect.heightProperty().bind(tile.heightProperty());
-
-            tile.getChildren().add(rect);
-
-            mainPane.getChildren().add(tile);
+            try {
+                tile = FXMLLoader.load(getClass().getResource("source-tile.fxml"));
+                sourceScrollBox.getChildren().add(tile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        sourceScrollPane.setPannable(true);
+        sourceScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sourceScrollPane.setFitToWidth(true);
+
+    }
+
+    public void loadAccountData() {
+
+        for (int i = 0; i < 20; i++) {
+            VBox tile;
+
+            try {
+                tile = FXMLLoader.load(getClass().getResource("account-tile.fxml"));
+                accountScrollBox.getChildren().add(tile);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        accountScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        accountScrollPane.setFitToWidth(true);
     }
 }
--- Desktop/PassApp/src/sample/Main.java
+++ Desktop/PassApp/src/sample/Main.java
@@ -4,17 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("new-main.fxml"));
         primaryStage.setTitle("Pass App");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
